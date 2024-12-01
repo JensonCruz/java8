f4aa3ff937bbc98d8d758d6e0ef7e3e8d2828482
Jenson CRUZ S <jenson.cruzs.tec@starhub.com>
2024-12-01 09:46:13 +0530

    Code changes for supplier, consumer and predicate

--- src/main/java/com/jenson/java8/CSP/SupplierDemo.java
+++ src/main/java/com/jenson/java8/CSP/SupplierDemo.java
@@ -11,6 +11,6 @@ public class SupplierDemo {
 
 		List<String> list1 = Arrays.asList();
 
-		System.out.println(list1.stream().findAny().orElseGet(() -> "Hi viewers"));
+		System.out.println(list1.stream().findAny().orElseGet(() -> "default message"));
 	}
 }
